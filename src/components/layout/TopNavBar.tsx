8c858757c057ed47f2728d7ef4ddd2319a4138a7
jeongbam <bellajw292@gmail.com>
2026-02-22 23:43:36 +0900

    refactor: 로고 클릭 시 홈 이동 기능 추가

--- src/components/layout/TopNavBar.tsx
+++ src/components/layout/TopNavBar.tsx
@@ -2,17 +2,24 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import MenuDrawer from "./MenuDrawer";
 
 export default function TopNavBar() {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
 
   return (
     <>
-      <header className="sticky top-0 bg-background">
+      <header className="fixed top-0 left-0 w-full bg-background z-[9999]">
         <div className="h-[10px]" />
+
         <div className="flex h-[60px] items-center justify-between px-5">
-          <div className="relative h-[30px] w-[80px]">
+          {/* 로고 */}
+          <div
+            className="relative h-[30px] w-[80px] cursor-pointer"
+            onClick={() => router.push("/home")}
+          >
             <Image
               src="/common/logo.svg"
               alt="Gread"
@@ -22,15 +29,22 @@ export default function TopNavBar() {
             />
           </div>
 
+          {/* 메뉴 버튼 */}
           <button
             type="button"
             aria-label="Open menu"
             onClick={() => setOpen(true)}
             className="flex h-10 w-10 items-center justify-center rounded-btn"
           >
-            <Image src="/common/icon-bar.svg" alt="" width={24} height={24} />
+            <Image
+              src="/common/icon-bar.svg"
+              alt="menu"
+              width={24}
+              height={24}
+            />
           </button>
         </div>
+
         <div className="h-[1.5px] bg-stroke" />
       </header>
 
